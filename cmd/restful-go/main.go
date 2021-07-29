f3e18b2a79ce29c3f11bfc92cadbd1c4c86a4ea3
Mikael Rozee <mikaelsrozee@gmail.com>
2021-07-29 14:42:50 +0100

    feat(req): get response based on ID

--- cmd/restful-go/main.go
+++ cmd/restful-go/main.go
@@ -10,12 +10,25 @@ import (
 )
 
 type QnrResponse struct {
+    Id string `json:"Id"`
     Name string `json:"Name"`
     Body string `json:"Body"`
     Email string `json:"Email"`
 }
 
 var QnrResponses []QnrResponse
+
+func getQnrResponse(w http.ResponseWriter, r *http.Request){
+    vars := mux.Vars(r)
+    key := vars["id"]
+
+    // Loop through each response, and if the ID matches, return the response
+    for _, response := range QnrResponses {
+        if response.Id == key {
+            json.NewEncoder(w).Encode(response)
+        }
+    }
+}
 
 func getAllQnrResponses(w http.ResponseWriter, r *http.Request){
     fmt.Println("Endpoint hit: all qnr responses")
@@ -26,15 +39,16 @@ func handleRequests() {
     router := mux.NewRouter().StrictSlash(true)
 
     router.HandleFunc("/responses", getAllQnrResponses)
+    router.HandleFunc("/responses/{id}", getQnrResponse)
 
     log.Fatal(http.ListenAndServe(":10000", router))
 }
 
 func main() {
     QnrResponses = []QnrResponse{
-        {Name: "Mikael Rozee", Body: "I am awesome.", Email: "foo@example.com"},
-        {Name: "Other Person", Body: "I am less awesome.", Email: "other@example.com"},
-        {Name: "Extra Dude", Body: "I am the least awesome.", Email: "extra@example.com"},
+        {Id: "1", Name: "Mikael Rozee", Body: "I am awesome.", Email: "foo@example.com"},
+        {Id: "2", Name: "Other Person", Body: "I am less awesome.", Email: "other@example.com"},
+        {Id: "3", Name: "Extra Dude", Body: "I am the least awesome.", Email: "extra@example.com"},
     }
     handleRequests()
 }
