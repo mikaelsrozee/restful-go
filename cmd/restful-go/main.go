92bc2317b3ee8775d88059a09a1d8fb57490e54b
Mikael Rozee <mikaelsrozee@gmail.com>
2021-07-29 14:31:40 +0100

    feat(req): send all responses on GET /

--- cmd/restful-go/main.go
+++ cmd/restful-go/main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func homepage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello world")
-    fmt.Println("Endpoint hit: homepage")
+type QnrResponse struct {
+    Name string `json:"Name"`
+    Body string `json:"Body"`
+    Email string `json:"Email"`
+}
+
+var QnrResponses []QnrResponse
+
+func getAllQnrResponses(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint hit: all qnr responses")
+    json.NewEncoder(w).Encode(QnrResponses)
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homepage)
+    http.HandleFunc("/responses", getAllQnrResponses)
     log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
+    QnrResponses = []QnrResponse{
+        {Name: "Mikael Rozee", Body: "I am awesome.", Email: "foo@example.com"},
+        {Name: "Other Person", Body: "I am less awesome.", Email: "other@example.com"},
+        {Name: "Extra Dude", Body: "I am the least awesome.", Email: "extra@example.com"},
+    }
     handleRequests()
 }
