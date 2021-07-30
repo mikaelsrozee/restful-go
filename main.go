93d769f8dc10a96b42f213f4c26cfd76f343a621
Mikael Rozee <mikaelsrozee@gmail.com>
2021-07-30 12:57:00 +0100

    feat(env): load db details from .env file

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+    // Load .env file
+    err := godotenv.Load()
+
+    if err != nil {
+        log.Fatal("Error loading .env file", err)
+    }
+
+    // Init app
     app := App{}
-    app.Init("mysql", "password", "test")
+    app.Init(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 }
